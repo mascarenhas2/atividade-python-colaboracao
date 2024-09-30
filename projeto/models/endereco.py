d4cc017e110231df0bfd37cf45e5cb54786ce453
Andrei Luiz de jesus <159576658+Andrei0769@users.noreply.github.com>
2024-09-30 13:02:44 +0000

    atualizado

--- projeto/models/endereco.py
+++ projeto/models/endereco.py
@@ -8,3 +8,4 @@ class Endereco():
         self.complemento = complemento
         self.cep = cep
         self.cidade = cidade
+
--- projeto/models/funcionario.py
+++ projeto/models/funcionario.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
+from projeto.models import endereco
 
 class Funcionario(ABC):
-    def __init__(self, nome : str, telefone: str, email:str, endereco: endereco) -> None:
+    def __init__(self, nome : str, telefone: str, email:str, endereco: endereco, salario:float) -> None:
         self.nome = nome
         self.telefone = telefone
         self.email = email
         self.endereco = endereco
+        self.salario = salario
