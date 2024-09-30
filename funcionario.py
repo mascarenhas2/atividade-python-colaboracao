f8e7b9f01998a42ba09abf30541ecd34bb9c6892
Andrei Luiz de jesus <159576658+Andrei0769@users.noreply.github.com>
2024-09-30 12:48:44 +0000

    up atv dupla

--- funcionario.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Funcionario(ABC):
-    def __init__(self, nome: str, telefone: str, email: str, endereco) -> None:
-        
- 
---
+++ projeto/models/endereco.py
@@ -0,0 +1,10 @@
+
+
+class Endereco(ABC):
+    def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: float):
+        # Inicializa os atributos do funcionário
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
