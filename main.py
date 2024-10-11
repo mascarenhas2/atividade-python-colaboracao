a524b45ec1650f86839dc25ed75cbfeb20e7217f
Andrei Luiz de jesus <159576658+Andrei0769@users.noreply.github.com>
2024-10-11 10:55:13 +0000

    Att

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Andrei Luiz de jesus Souza Almeida
-# Caio Vinicius Mascarenhas Costa
-
 import os
 os.system("cls || clear")
 
@@ -9,18 +6,21 @@ from projeto.models.engenheiro import Engenheiro
 from projeto.models.medico import Medico
 
 # Função para criar um endereço
-def criar_endereco():
-    return Endereco("Rua das Flores", "123", "Apto 101", "12345-678", "Salvador")
+def criar_endereco_engenheiro():
+    return Endereco("Rua Paracaina", "250", "|casa 145", "54321-678", "Salvador")
+
+def criar_endereco_medico():
+    return Endereco("Liberdade", "78", "|Apartamento 5B", "12345-678", "Salvador")
 
 # Função para criar um engenheiro
 def criar_engenheiro(endereco):
     return Engenheiro(
-        nome="Andrei Luiz",
+        nome="Andrei Luiz de Jesus Souza Almeida",
         telefone="988112455",
         email="andrei@2232.com",
-        crea="12345678",
+        crea="45488778",
         endereco=endereco,
-        salario=3000.00
+        salario=6815.10
     )
 
 # Função para criar um médico
@@ -31,21 +31,22 @@ def criar_medico(endereco):
         email="clara@medica.com",
         crm="98765432",
         endereco=endereco,
-        salario=5000.00  # Definindo um salário para o médico
+        salario=8512.20
     )
 
-# Criar o endereço
-endereco = criar_endereco()
-
+# Criar o endereço para o engenheiro
+endereco_engenheiro = criar_endereco_engenheiro()
 # Criar o engenheiro
-engenheiro = criar_engenheiro(endereco)
+engenheiro = criar_engenheiro(endereco_engenheiro)
 
 # Validar e imprimir informações do engenheiro
 engenheiro.validar_dados()
 print(engenheiro)
 
+# Criar o endereço para o médico
+endereco_medico = criar_endereco_medico()
 # Criar o médico
-medico = criar_medico(endereco)
+medico = criar_medico(endereco_medico)
 
 # Validar e imprimir informações do médico
 medico.validar_dados()
